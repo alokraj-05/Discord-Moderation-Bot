f2fef55af79504ea9412fd26e13a37f1ad241f1d
alokraj-05 <alok200504@gmail.com>
2024-08-17 22:11:50 +0530

    Updated custom status for the bot

--- src/events/ready.js
+++ src/events/ready.js
@@ -4,6 +4,10 @@ module.exports = {
   name: Events.ClientReady,
   once: true,
   execute(client) {
+    client.user.setPresence({
+      activities: [{ name: "help/aboutsergio", type: 2 }], // type 2 sets it as "Listening to"
+      status: "online", // Optional: You can set the bot's status to 'online', 'idle', 'dnd', or 'invisible'
+    });
     console.log(`Ready! Logged in as ${client.user.tag}`);
   },
 };
