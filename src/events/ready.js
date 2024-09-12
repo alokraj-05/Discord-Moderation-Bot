9060405ccf49e2f2af4d8b8e9a995fb1a85e2228
alokraj-05 <alok200504@gmail.com>
2024-09-13 00:45:02 +0530

    update the bot status via prefix command (integration with db)

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,11 +1,23 @@
 const { Events } = require("discord.js");
+const { getPrefix } = require("../prefix/getPrefix");
+const Status = require("../models/status");
 
 module.exports = {
   name: Events.ClientReady,
   once: true,
-  execute(client) {
+  async execute(client) {
+    const allGuilds = client.guilds.cache.map((guild) => guild.id);
+    if (!allGuilds.length) {
+      console.log("The bot is not in any guilds.");
+      return;
+    }
+    const savedStatus = await Status.findOne({});
+
+    const status = savedStatus ? savedStatus.status : "S!";
+    const activityType = savedStatus ? savedStatus.activityType : 2;
+
     client.user.setPresence({
-      activities: [{ name: "help/aboutsergio", type: 2 }], // type 2 sets it as "Listening to"
+      activities: [{ name: `${status}`, type: activityType }], // type 2 sets it as "Listening to"
       status: "online", // Optional: You can set the bot's status to 'online', 'idle', 'dnd', or 'invisible'
     });
     console.log(`Ready! Logged in as ${client.user.tag}`);
