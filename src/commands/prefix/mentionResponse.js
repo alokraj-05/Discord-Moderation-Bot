5438828079c10bdde00ac34b4fb253e835ed11ea
alokraj-05 <alok200504@gmail.com>
2024-09-22 18:41:05 +0530

    fixed bug in mention response

--- src/commands/prefix/mentionResponse.js
+++ src/commands/prefix/mentionResponse.js
@@ -1,21 +1,24 @@
-const { EmbedBuilder } = require('discord.js');
-const { getPrefix } = require('../../prefix/getPrefix'); // Assuming this is the correct path to fetch prefix
+const { EmbedBuilder } = require("discord.js");
+const { getPrefix } = require("../../prefix/getPrefix"); // Assuming this is the correct path to fetch prefix
 
 module.exports = {
-  name: 'mentionResponse',
+  name: "mentionResponse",
   async execute(message, client) {
-    if (message.author.bot || message.system) return;
+    if (
+      message.author.bot ||
+      message.system ||
+      !message.mentions.has(client.user)
+    )
+      return;
 
-    if (message.mentions.has(client.user)) {
+    if (message.mentions.users.has(client.user.id)) {
       const prefix = await getPrefix(message.guild.id);
-
-      // Create an embed response
       const embed = new EmbedBuilder()
-        .setColor('Blurple')
-        .setDescription(`<:3433mipinch:1284932155574714388> Please use my commands with \`${prefix}help\` or use /about to get about me || [documentation](https://sergio-docs.vercel.app/).`)
+        .setColor("Blurple")
+        .setDescription(
+          `<:3433mipinch:1284932155574714388> Please use my commands with \`${prefix}help\` or use /about to get about me || [documentation](https://sergio-docs.vercel.app/).`
+        )
         .setFooter({ text: `Prefix for this server: ${prefix}` });
-
-      // Send the embed in the same channel
       return message.reply({ embeds: [embed] });
     }
   },
