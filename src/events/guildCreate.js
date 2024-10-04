1c93718f14aa912e53d3a644f694a6ca971e8696
alokraj-05 <alok200504@gmail.com>
2024-10-04 23:37:43 +0530

    New guild create event

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -1,4 +1,6 @@
+const { ChannelType, PermissionsBitField } = require("discord.js");
 const { fetchAndSaveGuildData } = require("./FetchGuilds");
+const { PermissionOverwrites } = require("discord.js");
 
 module.exports = {
   name: "guildCreate",
@@ -7,5 +9,40 @@ module.exports = {
 
     // Fetch and save data for the new guild
     await fetchAndSaveGuildData(guild);
+
+    try {
+      const sergioUpdates = await guild.channels.create({
+        name: "sergio-updates",
+        type: 4,
+      });
+      const isCommunity = guild.features.includes("COMMUNITY");
+      if (isCommunity) {
+        await guild.channels.create({
+          name: "sergio-announcements",
+          parent: sergioUpdates.id,
+          type: 5,
+          permissionOverwrites: [
+            {
+              id: guild.roles.everyone.id,
+              deny: [PermissionsBitField.Flags.SendMessages],
+            },
+          ],
+        });
+      } else {
+        await guild.channels.create({
+          name: "sergio-announcements",
+          parent: sergioUpdates.id,
+          type: 0,
+          permissionOverwrites: [
+            {
+              id: guild.roles.everyone.id,
+              deny: [PermissionsBitField.Flags.SendMessages],
+            },
+          ],
+        });
+      }
+    } catch (error) {
+      console.error(`Error while setting up new category ${error.message}`);
+    }
   },
 };
