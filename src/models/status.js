dd92986adf906e36ba46b2d3c46cab0b414b15a9
alokraj-05 <alok200504@gmail.com>
2024-10-13 21:49:30 +0530

    Status db model

--- src/models/status.js
+++ src/models/status.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const statusSchema = new mongoose.Schema({
   status: { type: String, required: true },
   activityType: { type: Number, default: 2 },
+  mode: { type: String, default: "dnd" },
 });
 
 module.exports = mongoose.model("Status", statusSchema);
