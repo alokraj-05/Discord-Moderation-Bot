52b30f6f503554168b0a6f197e501c91fe34ea4c
alokraj-05 <alok200504@gmail.com>
2025-04-28 22:01:37 +0530

    Specific log with type eg. kick,leave

--- src/events/guild/leaveLog.js
+++ src/events/guild/leaveLog.js
@@ -3,12 +3,23 @@ const ChannelAlert = require("../../utils/channelAlert");
 module.exports = {
   name: "guildMemberRemove",
   async execute(member) {
+    const auditLogs = await member.guild.fetchAuditLog({
+      type: 20,
+      limit: 1,
+    });
+    const kickLog = auditLogs.entries.first();
     const leaveLogChannel = member.guild.channels.cache.find(
       (c) => c.name === "leave-log"
     );
+    const channelAlert = new ChannelAlert(leaveLogChannel);
     if (leaveLogChannel) {
-      const channelAlert = new ChannelAlert(leaveLogChannel);
-      channelAlert.dangerAlert(`<@${member.id}>, left the server.`);
+      return await channelAlert.dangerAlert(
+        `User: <${member.id}>, left the server.`
+      );
+    } else if (kickLog && leaveLogChannel) {
+      await channelAlert.dangerAlert(
+        `Action: Kick\nUser: <@${member.id}>\nKicked by: ${kickLog.executor.tag}`
+      );
     }
   },
 };
